df3c61aec2d9bf897d0b4fdf1426f03ab45d6e16
AlessandroGia <alexgiacento@gmail.com>
2023-05-04 14:22:24 +0200

    bug fixes

--- src/cogs/Mod.py
+++ src/cogs/Mod.py
@@ -8,7 +8,7 @@ from src.quotes.Quotes import Quotes
 
 
 class Law(ext.commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.__bot: commands.Bot = bot
         self.__leveling: Leveling = Leveling()
         self.__quotes: Quotes = Quotes()
@@ -17,10 +17,10 @@ class Law(ext.commands.Cog):
         self.__role_join: str = 'Yokai'
 
     @staticmethod
-    def __is_owner(interaction: Interaction):
+    def __is_owner(interaction: Interaction) -> bool:
         return interaction.user.id == interaction.guild.owner.id
 
-    def __id_bot(self, interaction: Interaction):
+    def __id_bot(self, interaction: Interaction) -> bool:
         return interaction.user.id == self.__bot.application_id
 
     @app_commands.command(
@@ -31,7 +31,7 @@ class Law(ext.commands.Cog):
         id='User id',
         points='Number of points'
     )
-    async def set_points(self, interaction: Interaction, id: str, points: int):
+    async def set_points(self, interaction: Interaction, id: str, points: int) -> None:
         if self.__is_owner(interaction):
             await self.__stats.set_user_points(interaction, int(id), points)
 
@@ -39,10 +39,10 @@ class Law(ext.commands.Cog):
         name='points',
         description='Get user points.'
     )
-    async def points(self, interaction: Interaction):
+    async def points(self, interaction: Interaction) -> None:
         await self.__stats.get_user_points(interaction)
 
-    async def __set_afk(self, message: Message):
+    async def __set_afk(self, message: Message) -> None:
         author_id: int = message.author.id
         owner: Member = message.guild.owner
         if self.__bot.application_id != author_id and owner.id != author_id and (
@@ -52,7 +52,7 @@ class Law(ext.commands.Cog):
                 f'{message.author.mention} {self.__quotes.get_random(self.__quotes.quotes_on_leave)}'
             )
 
-    async def __ping_bot(self, message: Message):
+    async def __ping_bot(self, message: Message) -> None:
         if self.__bot.application.id in [mem.id for mem in message.mentions] and not self.__bot.application_id == message.author.id:
             if message.author.id == message.guild.owner_id:
                 await message.channel.send(f'{message.author.mention} HAI!')
@@ -62,22 +62,28 @@ class Law(ext.commands.Cog):
                 )
 
     @commands.Cog.listener()
-    async def on_member_join(self, member: Member):
+    async def on_message(self, message: Message) -> None:
+        await self.__set_afk(message)
+        await self.__ping_bot(message)
+        await self.__leveling.exp(message)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: Member) -> None:
         role = discord.utils.get(member.guild.roles, name=self.__role_join)
         channel = member.guild.get_channel(self.__channel_bot)
         await member.add_roles(role)
         await channel.send(f'{member.mention} {self.__quotes.get_random(self.__quotes.quotes_on_join)}')
 
     @commands.Cog.listener()
-    async def on_member_remove(self, member: Member):
+    async def on_member_remove(self, member: Member) -> None:
         channel = member.guild.get_channel(self.__channel_bot)
         await channel.send(f'{member.name} {self.__quotes.get_random(self.__quotes.quotes_on_leave)}')
 
     @commands.Cog.listener()
-    async def on_member_ban(self, member: Member):
+    async def on_member_ban(self, member: Member) -> None:
         channel = member.guild.get_channel(self.__channel_bot)
         await channel.send(f'{member.name} {self.__quotes.get_random(self.__quotes.quotes_on_ban)}')
 
 
-async def setup(bot: ext.commands.Bot):
+async def setup(bot: ext.commands.Bot) -> None:
     await bot.add_cog(Law(bot), guilds=[Object(id=928785387239915540)])
