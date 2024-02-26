eecd78347e51a031a13a3ec602704d6de435d4db
AlessandroGia <alexgiacento@gmail.com>
2024-02-26 19:22:14 +0100

    updated pickline

--- src/voicestate/PickLine.py
+++ src/voicestate/PickLine.py
@@ -1,8 +1,15 @@
+import os
+
 class PickLine:
     def __init__(self) -> None:
+        self.__voice_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'voice', 'tyr')
         pass
 
     def pick(self) -> tuple[str, str]:
 
 
+
     def getAvatarByLine(self, line: str):
+
+
+    def getLine(self):
