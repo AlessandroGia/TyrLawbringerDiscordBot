46740948c28a521fbfb64dceefbba6eb28816443
alessandrogia <alexgiacento@gmail.com>
2024-08-16 16:29:41 +0200

    bug fix

--- src/cogs/Moderator.py
+++ src/cogs/Moderator.py
@@ -11,57 +11,66 @@ class Law(ext.commands.Cog):
         self.__bot: commands.Bot = bot
         self.__quotes: Quotes = Quotes()
 
-        self.__channel_bot: int = 1098908138570256464
+        self.__welcome_channel: int = 1098908138570256464
         self.__id_bot_role: int = 1006641555421016227
-        self.__role_join: str = 'Yokai'
-
-    def __id_bot(self, interaction: Interaction) -> bool:
-        return interaction.user.id == self.__bot.application_id
+        self.__role_join: int = 928793792994213908
 
     async def __set_afk(self, message: Message) -> None:
-        author_id: int = message.author.id
         owner: Member = message.guild.owner
-        if self.__bot.application_id != author_id and owner.id != author_id and (
+        if owner.id != message.author.id and (
                 owner.status == discord.Status.idle or owner.status == discord.Status.offline or owner.status == discord.Status.dnd
         ) and owner in message.mentions:
-            await message.channel.send(
-                f'{message.author.mention} {self.__quotes.get_random(self.__quotes.quotes_on_leave)}'
+            await message.reply(
+                content=self.__quotes.get_random(self.__quotes.quotes_on_leave),
+                mention_author=True
             )
 
     async def __ping_bot(self, message: Message) -> None:
         id_mentions = [mes.id for mes in message.mentions]
         id_role_mentions = [role.id for role in message.role_mentions]
-        if self.__bot.application_id in id_mentions or self.__id_bot_role in id_role_mentions \
-            and not self.__bot.application_id == message.author.id:
+        if self.__bot.application_id in id_mentions or self.__id_bot_role in id_role_mentions:
             if message.author.id == message.guild.owner_id:
-                await message.channel.send(f'{message.author.mention} HAI!')
+                await message.reply(
+                    content=f'HAI!',
+                    mention_author=True
+                )
             else:
-                await message.channel.send(
-                    f'{message.author.mention} {self.__quotes.get_random(self.__quotes.quotes_on_ban)}'
+                await message.reply(
+                    content=self.__quotes.get_random(self.__quotes.quotes_on_ping),
+                    mention_author=True
                 )
 
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
-        if message:
+        if message and not message.author.bot:
             await self.__set_afk(message)
             await self.__ping_bot(message)
 
     @commands.Cog.listener()
     async def on_member_join(self, member: Member) -> None:
-        role = discord.utils.get(member.guild.roles, name=self.__role_join)
-        channel = member.guild.get_channel(self.__channel_bot)
+        role = member.guild.get_role(self.__role_join)
+        channel = member.guild.get_channel(self.__welcome_channel)
         await member.add_roles(role)
-        await channel.send(f'{member.mention} {self.__quotes.get_random(self.__quotes.quotes_on_join)}')
+        await channel.send(
+            content=self.__quotes.get_random(self.__quotes.quotes_on_join),
+            mention_author=True
+        )
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: Member) -> None:
-        channel = member.guild.get_channel(self.__channel_bot)
-        await channel.send(f'{member.name} {self.__quotes.get_random(self.__quotes.quotes_on_leave)}')
+        channel = member.guild.get_channel(self.__welcome_channel)
+        await channel.send(
+            content=self.__quotes.get_random(self.__quotes.quotes_on_leave),
+            mention_author=True
+        )
 
     @commands.Cog.listener()
     async def on_member_ban(self, member: Member) -> None:
-        channel = member.guild.get_channel(self.__channel_bot)
-        await channel.send(f'{member.name} {self.__quotes.get_random(self.__quotes.quotes_on_ban)}')
+        channel = member.guild.get_channel(self.__welcome_channel)
+        await channel.send(
+            content=self.__quotes.get_random(self.__quotes.quotes_on_ban),
+            mention_author=True
+        )
 
 
 async def setup(bot: ext.commands.Bot) -> None:
--- src/quotes/Quotes.py
+++ src/quotes/Quotes.py
@@ -3,18 +3,30 @@ import random
 
 class Quotes:
 
-    quotes_on_leave: list = [
-        "No, that's no.", "That\'s no funny.", "Idiocy is not a defense.", "You cannot stop justice!",
-        "Begone foul creature!", "The wicked fall before me!",
-    ]
-
     quotes_on_join: list = [
         "HAI!"
     ]
 
+    quotes_on_leave: list = [
+        "No, that's no.",
+        "That's no funny.",
+        "Idiocy is not a defense.",
+        "You cannot stop justice!",
+        "Begone foul creature!",
+        "The wicked fall before me!",
+    ]
+
     quotes_on_ban: list = [
-        "We don't have time for jokes!", "You chose this, remember that.",
+        "We don't have time for jokes!",
+        "You chose this, remember that.",
         "I take no joy in what must be done!"
+    ]
+
+    quotes_on_ping: list = [
+        "We don't have time for jokes!",
+        "Idiocy is not a defense.",
+        "I take no joy in what must be done!",
+        "That\'s no funny."
     ]
 
     @staticmethod
